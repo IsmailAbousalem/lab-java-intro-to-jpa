406528b96afda9a5efd70fb8ffb3f805ce326e8a
Ismail <ia.abousalem@gmail.com>
2024-06-26 12:27:24 -0400

    created test class and added git repo in spring project

---
+++ src/main/java/com/IntroToJPA/Lab304IntroToJPA/Lab304IntroToJpaApplication.java
@@ -0,0 +1,13 @@
+package com.IntroToJPA.Lab304IntroToJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab304IntroToJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab304IntroToJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/IntroToJPA/Lab304IntroToJPA/TestClass.java
@@ -0,0 +1,4 @@
+package com.IntroToJPA.Lab304IntroToJPA;
+
+public class TestClass {
+}
